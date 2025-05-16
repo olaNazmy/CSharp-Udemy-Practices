007896b1ce1840d892e85d1b3832f36b25c8252d
ola <olanazmy@gmail.com>
2025-05-16 21:29:46 +0300

    Done

--- Sorting.Array.BubbleSort/Program.cs
+++ Sorting.Array.BubbleSort/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-           
+            //define array
+            int[] numbers = { 200, 80, 108, 44, 90, 77, 33, 50, 41 };
+            int swap;
+
+            Console.WriteLine("Array before Sorting : ");
+            foreach (int item in numbers)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine("\n-------------------------------------");
+            for (int k = 0; k < numbers.Length - 1; k++)
+            {
+                for (int i = 0; i < numbers.Length - 1; i++)
+                {
+                    if (numbers[i] > numbers[i + 1])
+                    {
+                        //use swap temporary
+                        swap = numbers[i + 1];
+                        numbers[i + 1] = numbers[i];
+                        numbers[i] = swap;
+                    }
+                }
+            }
+
+            Console.WriteLine("Array after Sorting : ");
+            foreach (int item in numbers)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine("\n");
         }
     }
 }
